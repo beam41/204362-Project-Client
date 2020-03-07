23b5263dfe78d136b6ee2e055e054574a6ac8603
beam41 <beam_baimon@hotmail.com>
2020-03-07 20:09:57 +0700

    add div in listpage

--- src/components/Shared/ListPage.vue
+++ src/components/Shared/ListPage.vue
@@ -1,13 +1,11 @@
 <template>
-  
+  <div></div>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import Vue from 'vue';
 
-}
+export default Vue.extend({});
 </script>
 
-<style>
-
-</style>
+<style></style>
