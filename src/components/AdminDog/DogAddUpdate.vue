5cfdadd407f142fdafacbdda1b79a215c2978b36
Juthapon Simmalee <idea_par@hotmail.com>
2020-03-10 14:07:48 +0700

    add data  to input filed

--- src/components/AdminDog/DogAddUpdate.vue
+++ src/components/AdminDog/DogAddUpdate.vue
@@ -1,63 +1,69 @@
 <template>
   <div class="addupdate">
-    <div></div>
-    <div>Dog ID:</div>
+    <div>
+      <div>Dog ID: {{ dog.id }}</div>
 
-    <div class="form-control">
-      <label>ชื่อ: </label>
-      <input type="text" placeholder="Input Text" />
+      <div class="form-control">
+        <label>ชื่อ:</label>
+        <input type="text" placeholder="กรุณากรอกชื่อ" v-model="dog.Name" />
+      </div>
+
+      <div class="form-control">
+        <label>พันธุ์:</label>
+        <input type="text" placeholder="กรุณากรอกพันธุ์" v-model="dog.Breed" />
+      </div>
+
+      <div class="form-control">
+        <label>อายุ:</label>
+        <input type="text" placeholder="" style="width: 50px" />  ปี
+        <input type="text" placeholder="" style="width: 50px" />  เดือน
+      </div>
+
+      <div class="form-control">
+        <label>เพศ:</label>
+        <span class="select" style="width: 200px">
+          <Select
+            customText="กรุณาเลือกเพศ"
+            :options="['Mee', 'Moo', 'Maa']"
+            @change="log($event)"
+          />
+        </span>
+      </div>
+
+      <div class="form-control">
+        <label>ลักษณะ:</label>
+        <textarea v-model="dog.Description"></textarea>
+      </div>
+
+      <div class="form-control">
+        <label>สีปลอกคอ:</label>
+        <span class="select" style="width: 200px">
+          <Select
+            customText="กรุณาเลือกสีปลอกคอ"
+            :options="['Mee', 'Moo', 'Maa']"
+            @change="log($event)"
+          />
+        </span>
+      </div>
     </div>
 
-    <div class="form-control">
-      <label>พันธุ์: </label>
-      <input type="text" placeholder="Input Text" />
-    </div>
+    <div>
+      <div class="form-control">
+        <label>ติดต่อ:</label>
+        <input type="text" placeholder="กรุณากรอกข้อมูลติดต่อ" v-model="dog.CaretakerPhone" />
+      </div>
 
-    <div class="form-control">
-      <label>อายุ: </label>
-      <input type="text" placeholder="Input Text" />
-    </div>
+      <div class="form-control">
+        <label>ผู้ดูแล:</label>
+        <input type="text" placeholder="Input Text" v-model="dog.Caretaker" />
+      </div>
 
-    <div class="form-control">
-      <label>เพศ: </label>
-      <span class="select" style="width: 200px">
-        <Select
-          customText="Select Your Mee:"
-          :options="['Mee', 'Moo', 'Maa']"
-          @change="log($event)"
-        />
-      </span>
-    </div>
-
-    <div class="form-control">
-      <label>ลักษณะ: </label>
-      <input type="text" placeholder="Input Text" />
-    </div>
-
-    <div class="form-control">
-      <label>สีปลอกคอ: </label>
-      <span class="select" style="width: 200px">
-        <Select
-          customText="Select Your Mee:"
-          :options="['Mee', 'Moo', 'Maa']"
-          @change="log($event)"
-        />
-      </span>
-    </div>
-
-    <div class="form-control">
-      <label>ติดต่อ: </label>
-      <input type="text" placeholder="Input Text" />
-    </div>
-
-    <div class="form-control">
-      <label>ผู้ดูแล: </label>
-      <input type="text" placeholder="Input Text" />
-    </div>
-
-    <div class="form-control">
-      <label>ที่อยู่: </label>
-      <input type="text" placeholder="Input Text" />
+      <div class="form-control">
+        <label>ที่อยู่:</label>
+        <textarea v-model="dog.Location"></textarea>
+      </div>
+      <button class="btn-success" href="#" >save</button>
+      <button class="btn-warn" href="#" >Delete</button>
     </div>
   </div>
 </template>
@@ -69,16 +75,21 @@ import DogApiService from '@/services/DogApiService';
 export default {
   name: 'DogAddUpdate',
   data: () => ({
-    dogs: null,
+    dog: null,
   }),
   components: {
     Select,
   },
   created() {
-    console.log(DogApiService.getDog(+this.$route.params.id));
-    this.dogs = DogApiService.getDog(+this.$route.params.id);
+    this.dog = DogApiService.getDog(+this.$route.params.id);
   },
 };
 </script>
 
-<style></style>
+<style>
+.addupdate {
+  display: grid;
+  grid-template-columns: 60fr 40fr;
+}
+
+</style>
