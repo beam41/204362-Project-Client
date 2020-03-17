ba44aa87e31c8e2a4643ef29ed2b0811bfe61fff
Juthapon Simmalee <idea_par@hotmail.com>
2020-03-17 18:43:42 +0700

    not yet

--- src/components/Admin/Dog/DogAddUpdate.ts
+++ src/components/Admin/Dog/DogAddUpdate.ts
@@ -128,11 +128,11 @@ export default Vue.extend({
       // @ts-ignore
       const location = this.$refs.location.value;
       // Check condition Err
-      if (name === '' || /[\d!"#$%&\\'*+-./:;<=>?@[\\\]^_`{|}~]/gm.test(name)) {
+      if (name === '' || /[\d!"#$%&\\'*+./:;<=>?@[\\\]^_`{|}~-]/gm.test(name)) {
         this.nameErr = true;
         err = true;
       }
-      if (breed === '' || /[\d!"#$%&\\'()*+,-./:;<=>?@[\\\]^_`{|}~]/gm.test(breed)) {
+      if (breed === '' || /[\d!"#$%&\\'()*+,-./:;<=>?@[\\\]^_`{|}~-]/gm.test(breed)) {
         this.breedErr = true;
         err = true;
       }
@@ -169,7 +169,7 @@ export default Vue.extend({
         this.caretakerPhoneErr = true;
         err = true;
       }
-      if (caretaker === '' || /[\d!"#$%&\\'()*+,-/:;<=>?@[\\\]^_`{|}~]/gm.test(caretaker)) {
+      if (caretaker === '' || /[\d!"#$%&\\'()*+,/:;<=>?@[\\\]^_`{|}~-]/gm.test(caretaker)) {
         this.caretakerErr = true;
         err = true;
       }
